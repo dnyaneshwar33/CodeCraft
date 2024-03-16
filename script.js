c42d90ba787192f4ec355e7c3ec3dc2adaf64e2c
Dnyaneshwar Vighne <125205721+dnyaneshwar33@users.noreply.github.com>
2024-03-16 16:47:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+    // window.onscroll = function () {
+    //     makeNavSticky();
+    // };
+
+    // function makeNavSticky() {
+    //     var nav = document.getElementById("main-nav");
+
+    //     if (window.pageYOffset > nav.offsetTop) {
+    //         nav.classList.add("sticky");
+    //     } else {
+    //         nav.classList.remove("sticky");
+    //     }
+    // }
+
+
+    // -------------
+    function toggleMobileNav() {
+        var mobileNav = document.getElementById("mobileNav");
+        mobileNav.classList.toggle("show");
+    }
+    
